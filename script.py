058899c60d8e2d47d9c5feb39d2b84739aabb8c8
Justin Lee <justin.r.lee@gmail.com>
2019-02-11 12:14:21 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,21 @@
+import time
+
+print("Starting python script...")
+print("Processing code (runs for 30 seconds)...")
+
+time.sleep(6)
+print("20%")
+
+time.sleep(6)
+print("40%")
+
+time.sleep(6)
+print("20%")
+
+time.sleep(6)
+print("80%")
+
+time.sleep(6)
+print("100%")
+
+print("Done!")
